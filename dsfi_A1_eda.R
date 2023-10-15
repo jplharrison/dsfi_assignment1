3b880de047ffa4d0f2de00dae364e88ccf353533
jplharrison <jplh34@gmail.com>
2023-10-15 12:55:21 +0200

    Render Quarto

---
+++ dsfi_A1_eda.R
@@ -0,0 +1,26 @@
+libraries <- 'libraries.R'
+source(libraries)
+setpwd()
+
+sentence_lengths <- sona_sentences %>% group_by(president_speaker, year) %>% 
+  mutate(mean_count = mean(word_count)) %>%
+  select(president_speaker, year, mean_count) %>% ungroup()
+
+plot(sentence_lengths$year, sentence_lengths$mean_count, 
+     col=as.factor(sentence_lengths$president_speaker), pch =19, cex=2,
+     ylab='Word Count Per Sentence', xlab='Year')
+legend('topright', legend=unique(sentence_lengths$president_speaker),
+       col=as.factor(unique(sentence_lengths$president_speaker)), 
+       pch =19, cex=0.95, bty='n')
+
+name_counts <- sentence_dict %>% 
+  filter(word %in% c('mandela','zuma','klerk','motlanthe', 'mbeki','ramaphosa')) %>% 
+  #'nelson','jacob','thabo',,'cyril'
+  inner_join(sona_sentences) %>% 
+  group_by(president_speaker, word) %>% count() %>% ungroup() %>% 
+  select(president_speaker, word, n) %>%
+  pivot_wider(names_from = word, values_from = n, values_fill = 0) 
+
+name_mat <- as.matrix(name_counts[,-1]); row.names(name_mat) <- name_counts$president_speaker;
+barplot(t(name_mat), beside=T,legend.text = colnames(name_mat),
+        args.legend = list(x='top', bty='n')) 
--- libraries.R
+++ libraries.R
@@ -1,16 +1,8 @@
 rm(list=ls())
 setpwd <- function(){setwd(dirname(rstudioapi::getActiveDocumentContext()$path))}
 library(reticulate)
-#use_python("C:/Users/User/AppData/Local/Programs/Python/Python311/python.exe", required = TRUE)
 library(keras)
-#install_keras()
 library(tensorflow)
-#install_tensorflow()
-#install_tensorflow(envname = "r-pytensorflow")
-#use_virtualenv("r-pytensorflow", required = T)
-#use_virtualenv("r-tensorflow", required = T)
-#use_virtualenv("C:/Users/User/anaconda3/envs/r-tensorflow/python.exe", required = T)
-#install_keras(envname = "r-pytensorflow")
 library(tidyverse)
 library(ggplot2)
 library(plotly)
@@ -23,7 +15,7 @@ library(xgboost)
 library(nnet)
 library(caret)
 library(Metrics)
-
+library(e1071)
 
 
 
